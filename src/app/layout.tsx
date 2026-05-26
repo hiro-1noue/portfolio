c87a954079c9f2913b108a9861fce5ec5db9346b
hiro <hiro.sec@outlook.jp>
2026-05-27 08:43:19 +0900

    Reset layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Portfolio",
   description: "This website is portfolio of hiro-1noue",
@@ -27,7 +17,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      	<Header />
+      	
+	<main>
+      	    {children}
+	</main>
+
+	<Footer />
+      </body>
     </html>
   );
 }
