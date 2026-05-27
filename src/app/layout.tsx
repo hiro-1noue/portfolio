a4ff995470315543625d41c307b0233ed987bf08
hiro <hiro.sec@outlook.jp>
2026-05-27 16:37:17 +0900

    Fix import Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import "./components/Footer.tsx"
+import Footer from "./components/Footer.tsx"
 import "./globals.css";
 
 export const metadata: Metadata = {
