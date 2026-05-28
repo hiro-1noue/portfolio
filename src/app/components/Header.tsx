87125a6277b52f98830455fc1c71818f662daef2
hiro <hiro.sec@outlook.jp>
2026-05-28 17:04:50 +0900

    Change header layout

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,10 +5,22 @@ type HeaderProbs = {
 export default function Header({ title }: HeaderProbs) {
   return (
     <header
-      className="fixed top-0 z-50 w-full h-14 m-3"   
+      className="fixed top-0 z-50 w-full h-14 p-3"   
     >
-      <div className="w-auto max-w-50 bg-white backddrop-blur px-5 py-2">
-        <h1>{title}</h1>
+      <div className="flex w-full h-full justify-between items-center">
+        <div className="text-white">
+          <a href="#">portfolio</a>
+        </div>
+
+        <div className="flex justify-between items-center w-auto max-w-100 h-auto bg-white backdrop-blur px-5 py-2 gap-4 rounded">
+	  <div className="">Home</div>
+	  <div>Work</div>
+	  <div>Profile</div>
+        </div>
+
+	<div className="text-white">
+	  <div>Contact</div>
+	</div>
       </div>
     </header>
   );
