4ff58fd2a9066ea231ff504c0c18d8d9ad2bbc97
hiro <hiro.sec@outlook.jp>
2026-05-28 17:39:57 +0900

    Fix typos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
+import type { Metadata } from "next";
 import Header from "./components/Header";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import "./globals.css";
 
 export const metadata: Metadata = {
