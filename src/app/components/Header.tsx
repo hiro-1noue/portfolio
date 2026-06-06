be9a1d16b1755ffa43f1b29a79b32123d2ae4ade
hiro <hiro.sec@outlook.jp>
2026-06-06 22:41:22 +0900

    Update contact

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,19 +14,12 @@ export default function Header({ title }: HeaderProps) {
           <Link href="/">portfolio</Link>
         </div>
 
-	<div className="flex justify-between items-center gap-4">
-
-          <nav className="flex justify-between items-center w-auto max-w-100 h-auto px-5 py-2 gap-4 rounded">
-	    <Link href="/" className="">Home</Link>
-	    <Link href="/works">Works</Link>
-	    <Link href="/profile">Profile</Link>
-          </nav>
-
-	  <div className="text-white">
-	    <div>Contact</div>
-	  </div>
-
-	</div>
+        <nav className="flex justify-between items-center w-auto max-w-100 h-auto px-5 py-2 gap-4 rounded">
+	  <Link href="/" className="">Home</Link>
+	  <Link href="/works">Works</Link>
+	  <Link href="/profile">Profile</Link>
+	  <Link href="/contact" className="text-white bg-black px-5 py-4 rounded-b-sm">Contact</Link>
+        </nav>
 
       </div>
     </header>
