a0c685c7bff0d7ff7851fce1c92215458bf1eece
hiro <hiro.sec@outlook.jp>
2026-06-08 16:14:04 +0900

    Update about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,35 @@ import Hero3D from "./components/Hero3D";
 
 export default function Home() {
   return (
-    <section id="hero" className="relative w-full h-screen overflow-hidden">
-      <Hero3D />
+    <>
+      <section id="hero" className="relative w-full h-screen overflow-hidden">
+        <Hero3D />
 
-      <div className="relative w-full h-full z-10">
-        <h1 className="text-4xl absolute top-1/2 pl-4">Hiro's Portfolio</h1>
-      </div>
-    </section>
+        <div className="relative w-full h-full z-10">
+          <h1 className="text-4xl absolute top-1/2 left-4">Hiro's Portfolio</h1>
+        </div>
+      </section>
+
+      <section className="max-w-5xl mx-auto my-16 px-4 border-t relative overflow-hidden">
+        <h2 className="text-3xl my-6">About</h2>
+	<div className="leading-8">
+	  <p>
+	    I'm active under the name Hiro on SNS.
+	  </p>
+	  <p>
+	    I'm a student interested in web development, cybersecurity and competitive programming.
+	  </p>
+	</div>
+      </section>
+
+      <section className="max-w-5xl mx-auto my- px-4">
+        <h2 className="text-3xl mb-8">Featured Works</h2>
+
+	<div className="grid md:grid-cols-2 gap-6">
+	   <WorkCard />
+	   <WorkCard />
+	</div>
+      </section>
+    </>
   );
 }
