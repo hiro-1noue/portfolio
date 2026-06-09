374209d7aed3449ec819492342c458bfa5ed7f33
hiro <hiro.sec@outlook.jp>
2026-06-09 12:02:43 +0900

    Add title on works

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 
 export default function Works() {
   return (
-	 <h1>works</h1>
+    <section className="px-4">
+      <h1 className="text-3xl pb-2 mb-4 border-b">works</h1>
+      <p>There are my works made since 2024</p>
+      <div className="grid md:gird-cols-2">
+      </div>
+    </section>
   );
 }
