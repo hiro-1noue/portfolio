9b4e325b9d9e984c28724608432fd5046037131a
hiro <hiro.sec@outlook.jp>
2026-06-26 16:45:07 +0900

    feat: add padding

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -4,7 +4,7 @@ import WorkCard from "../components/WorkCard";
 
 export default function Works() {
   return (
-    <section className="max-w-5xl mx-auto">
+    <section className="max-w-5xl mx-auto px-4">
       <h1 className="text-3xl pb-2 mb-4 border-b">Works</h1>
       <p>There are my works made since 2024</p>
       <div className="grid md:grid-cols-2">
