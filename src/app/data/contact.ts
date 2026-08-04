8d63af2e1c948c72ba8f7acc6d9ea5c5df048d87
hiro <hiro.sec@outlook.jp>
2026-08-04 20:47:28 +0900

    feat: Add how to contact me

---
+++ src/app/data/contact.ts
@@ -0,0 +1,8 @@
+export const contact = {
+  email: "hiro.1noue@outlook.jp",
+  sns: [
+    { label: "GitHub", href: "https://github.com/hiro-1noue" },
+    { label: "X", href: "https://x.com/hiro_1noue" },
+    { label: "Qiita", href: "https://qiita.com/hiro-1noue" },
+  ],
+};
